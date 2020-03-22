a170a4c4b4e2dd8474777104b37209ee5307b2ca
Roshan Sharma <reprogram46@gmail.com>
2020-03-22 22:19:53 +0530

    fix: Set CORS headers for incoming calls
    Allow all Access-Control-Allow-Origin and Access-Control-Allow-Headers.

--- api/__init__.py
+++ api/__init__.py
@@ -1,5 +1,14 @@
 import falcon
 
-app = falcon.API()
+
+class CORSMiddleware:
+    def process_response(self, req, resp, ressource, req_succeeded):
+        resp.set_header('Access-Control-Allow-Origin', '*')
+        resp.set_header('Access-Control-Allow-Headers', '*')
+
+
+app = falcon.API(middleware=[
+    CORSMiddleware(),
+])
 
 import api.routes    # noqa
