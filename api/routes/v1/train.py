fe924cc2edc27b297ffd692cec1fbb4bb465ea01
Roshan Sharma <reprogram46@gmail.com>
2020-03-23 02:12:23 +0530

    feat: Store request data in CSV
    Store the incoming request list as a new record in records.csv.

--- api/routes/v1/train.py
+++ api/routes/v1/train.py
@@ -1,9 +1,32 @@
+import json
+import falcon
+import numpy as np
+import pickle
+
 from api import app
+from api.lib.algorithms import resume_clf
 
 
-class TrainResource:
-    def on_put(self, req, resp):
-        resp.media = 'hello'
+class TrainModelResource:
+    def on_post(self, req, resp):
+        if len(req.media) > 10:
+            # Minimise any unneccessary processing the server would do for
+            # bad requests, thus reducing CPU use.
+            resp.status = falcon.HTTP_400
+            resp.media = json.dumps({
+                "status": "fail",
+            })
+        else:
+            N = len(req.media)
+            with open('api/records.csv', 'a') as rf:
+                for (idx, score) in enumerate(req.media):
+                    if idx < N - 1:
+                        rf.write(str(score) + ', ')
+                    elif idx == N - 1:
+                        rf.write(str(score) + '\n')
+            resp.media = json.dumps({
+                "status": "pass",
+            })
 
 
-app.add_route('/api/v1/train', TrainResource())
+app.add_route('/api/v1/train', TrainModelResource())
